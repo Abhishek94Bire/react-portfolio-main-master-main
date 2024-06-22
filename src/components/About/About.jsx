6dde65755441964d68c0ec357d36545bc17e8609
Abhishek94Bire <salesagroprollp@gmail.com>
2024-06-22 17:42:56 +0530

    new commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { techStack } from "../Constants";
+import { motion } from "framer-motion";
+import dpSai from "./dpSai1.jpg";
+import "../index.css";
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import Footer from "./Footer";
+
+export default function AboutMe() {
+  return (
+    <>
+      <div className="w-10/12 sm:w-8/12 container mx-auto mt-9">
+        <h1
+          className="text-3xl sm:text-4xl lg:text-5xl my-3"
+          style={{ color: "var(--primary)" }}
+        >
+          Hi, I'm Abhishek Bire👋
+        </h1>
+        <div className="info flex flex-col-reverse sm:grid sm:grid-cols-2">
+          <div className="info-text sm:span-col-8 lg:mt-5">
+            <p>
+              I'm a Full Stack Developer Trainee based in Bangalore, India. I'm
+              Completed  Full Stack Developer Programme at GUVI Zen-Class IITM
+              Research Park.
+            </p>
+            <br />
+            <p>
+              I've graduated from Rajiv Gandhi  College of Engineering, Chandrapur with
+              degree in Mechanical  Engineering. I always had fond of
+              learning new skills and technologies, that’s how I got into Web
+              Development while studying in college. I mainly use React for
+              frontend and NodeJS for backend.
+            </p>
+            <br />
+            <p>
+              When I am not coding, I like to explore about new smartphones and
+              gadgets, listen some music, play mobile games, play badminton,
+              learn new skills.
+            </p>
+
+            <div className="resume-button mt-6">
+              <a
+                href="https://drive.google.com/file/d/1tAUG259N9KS2KVc2Ab0irbV6FWT3cwlr/view"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button
+                  type="button"
+                  className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-md px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900 shadow-md hover:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Download Resume
+                  <FileDownloadOutlinedIcon className="ml-1" />
+                </button>
+              </a>
+            </div>
+          </div>
+          <img
+            //src={dpSai}
+            alt="dp"
+            className="relative mx-auto my-8 w-10/12 sm:w-1/2 lg:block img-wrapper justify-content sm:my-0 sm:span-col-4 lg:flex-wrap-reverse rounded-lg shadow-lg"
+          />
+        </div>
+
+        <div className="tech-list mt-9">
+          <h1
+            className="text-3xl sm:text-4xl lg:text-5xl my-3"
+            style={{ color: "var(--primary)" }}
+          >
+            Technologies I work with
+          </h1>
+          {
+            <div className="flex justify-center pb-6">
+              <motion.div className="flex flex-wrap mt-4 justify-center sm:justify-between ">
+                {techStack.map((el, index) => (
+                  <motion.div
+                    initial="hidden"
+                    whileInView={"visible"}
+                    variants={{
+                      visible: {
+                        y: 0,
+                        opacity: 1,
+                        transition: {
+                          type: "spring",
+                        },
+                      },
+                      hidden: { opacity: 1, y: 80 },
+                    }}
+                    className="px-3 py-2 m-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 rounded-lg flex items-center md:w-48 w-40 shadow-md cursor-pointer hover:shadow-lg hover:scale-105 ease-in-out"
+                    key={index}
+                    style={{ backgroundColor: "var(--backbox)" }}
+                  >
+                    <img alt={`pic${index}`} src={el.link} className="w-12" />
+                    <h4 className="text-md ml-3 text-center">{el.name}</h4>
+                  </motion.div>
+                ))}
+              </motion.div>
+            </div>
+          }
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,83 @@
+
+import { Container, Col, Row, Button } from 'react-bootstrap'
+import React, { useRef, useState } from 'react';
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import emailjs from '@emailjs/browser';
+ 
+
+export const Contact = () => {
+  const form = useRef();
+
+ 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cegx14m', 'template_ea06ixb', form.current, 'ELOpDhIKWVRENY1d6')
+      .then((result) => {
+        location.reload();
+
+          console.log(result.text);
+          
+      }, (error) => {
+          console.log(error.text);
+      });
+
+  };
+
+  return (
+
+<>
+
+    <Container style={{ padding: '100px' }} id='contact' >
+      <Row >
+        <Col md={6} className="c-left" >
+          <h1 className='fw-bold'>Get in Touch</h1>
+          <h1 className="yellow fw-bold">Contact me</h1>
+          <Container style={{ padding: '30px' }}>
+            <Row>
+              <Col md={12} className="contact-social text-center">
+
+                <a
+                  href="https://github.com/Abhishek94Bire"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-color text-black  contact-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+
+
+                <a
+                  href="https://www.linkedin.com/in/abhishek-bire-77357a257/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-color  contact-social-icons "
+                >
+                  <FaLinkedinIn className='text-primary' />
+                </a>
+
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+        <Col md={6} className="c-right">
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name="user_name" className='user' placeholder='Enter Your Name'  required/>
+            <input type="email" name="user_email" className='user' placeholder='Enter Your Email' required />
+            <textarea name="message" className='user' placeholder='Message' required  />
+            <Button type="submit" className="button ">Send</Button>
+          </form>
+        </Col>
+
+      </Row>
+
+
+    </Container>
+
+</>
+  );
+};
+
+
+
